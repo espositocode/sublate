a3b7b82e12560bb65f92c89ba3685eddef831888
Jonathan Esposito <jonathan@esposito.page>
2022-12-10 19:52:59 -0500

    Updated library usage with python functions

--- README.md
+++ README.md
@@ -1,30 +1,3 @@
-# DEPRECATED
+# Sublate
 
-Sublate (still in alpha development) is a tool for building template based projects with jinja. It can be used for blogs, scaffolds, [themes](https://github.com/subtheme-dev), or any kind of project that renders structured data. 
-
-To install:
-
-    $ pip install sublate
-
-## Quickstart
-
-Sublate works copying project files to an output directory and rendering the selected template files with the supplied data, as configured by a sublate file.
-
-Start by creating `sublate.yaml` at the root of your project, with the location to your data and template files. For example, this project will render `index.html` with the data supplied from `data.json`:
-
-```yaml
-# path to data file(s)
-# data will not be copied to the output directory
-data: "/data.yaml"
-
-# path to template file(s) that will be rendered
-render: "/index.html"
-```
-
-To build, just run `sublate` from the project directory. This will create an `output` directory with all project files, including the rendered `index.html`.
-
-    $ sublate
-
-## Data Inheritance
-
-Sublate will pass project data to each sub-directory, and data can also be configured from a sub-directory (with another sublate file). Any data configured from a child directory cannot be shared with its parent or sibling directory. All data from the parent is merged with the child, and the child will overwrite any conflicts.
+Sublate is a python library for building template projects.
--- sublate.py
+++ sublate.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-import argparse
-import fnmatch
 import json
 import os
 import shutil
@@ -10,180 +8,13 @@ import jinja2
 import yaml
 
 
-def main():
-    parser = argparse.ArgumentParser(prog='sublate')
-    parser.add_argument('path', metavar='path', type=str, nargs='?', default='.', help='path to project')
-    parser.add_argument('--data', metavar='data', type=str, nargs='*', help='path to data')
-    parser.add_argument('--output', metavar='output', type=str, nargs='?', default='output', help='path to output')
-    parser.add_argument('--render', metavar='render', type=str, nargs='*', help='files to render')
-    parser.add_argument('--remove', metavar='remove', type=str, nargs='*', help='files to remove')
-
-    args = parser.parse_args()
-
-    data = {}
-    if args.data:
-        for data in args.data:
-            data.update(get_project_data(args.data))
-
-    build(args.path, args.output, args.render, args.remove, data)
+data = {}
 
 
-# TODO: args.output should overwrite root sublate.yaml
-def build(path, output_path, data=None, render=None, remove=None):
-    if data is None:
-        data = {}
-
-    if render is None:
-        render = []
-
-    if remove is None:
-        remove = []
-
-    _build(path, output_path, data, render, remove, path)
+# python
 
 
-def _build(path, output_path, data, render, remove, root_path):
-    local_data = get_sublate_data(path)
-
-    if "path" in local_data:
-        path = os.path.join(path, local_data["path"])
-
-    if "output" in local_data:
-        if local_data["output"][0] == "/":
-            output_path = local_data["output"]
-        else:
-            output_path = os.path.join(path, local_data["output"])
-
-    if "data" in local_data:
-        if type(local_data["data"]) is list:
-            for d in local_data["data"]:
-                if d[0] == "/":
-                    data_path = d[0]
-                else:
-                    data_path = os.path.join(root_path, d["data"])
-
-                data.update(get_project_data(data_path))
-        else:
-            if local_data["data"][0] == "/":
-                data_path = local_data["data"][0]
-            else:
-                data_path = os.path.join(root_path, local_data["data"])
-
-            data.update(get_project_data(data_path))
-
-    if "remove" in local_data:
-        if type(local_data["remove"]) is list:
-            remove = local_data["remove"]
-        else:
-            remove = [local_data["remove"]]
-
-    if "render" in local_data:
-        if type(local_data["render"]) is list:
-            render = local_data["render"]
-        else:
-            render = [local_data["render"]]
-
-        root_path = path
-
-    if os.path.exists(output_path):
-        shutil.rmtree(output_path)
-
-    print(f"Building: {path}")
-    os.mkdir(output_path)
-
-    if "path" in local_data and local_data["path"] not in [".", "./"]:
-        _build(path, output_path, data, render, remove, root_path)
-        return
-
-    # TODO: skip over data and output paths
-    for filename in os.listdir(path):
-        if filename.startswith("sublate."):
-            continue
-
-        if filename in remove:
-            continue
-
-        full_path = os.path.join(path, filename)
-        full_output_path = os.path.join(output_path, filename)
-
-        if os.path.isdir(full_path):
-            local_render = []
-            for r in render:
-                parts = r.split(os.sep)
-                if filename == parts[0]:
-                    local_render.append(os.path.join(*parts[1:]))
-
-            _build(full_path, full_output_path, data, local_render, remove, root_path)
-        else:
-            for r in render:
-                if fnmatch.fnmatch(filename, r):
-                    env = jinja2.Environment(loader=jinja2.ChoiceLoader([
-                        jinja2.FileSystemLoader(searchpath=path),
-                        jinja2.FileSystemLoader(searchpath=root_path)
-                    ]))
-
-                    try:
-                        template = env.get_template(filename)
-                    except UnicodeDecodeError:
-                        print(f"Cannot render binary file: {full_path}")
-                        continue
-
-                    output = template.render(**data).strip()
-                    with open(full_output_path, 'w+') as f:
-                        f.write(output)
-
-                    break
-            else:
-                shutil.copy(full_path, full_output_path)
-
-
-def get_project_data(path):
-    data = {}
-
-    if os.path.isdir(path):
-        for filename in os.listdir(path):
-            data.update(load_path(os.path.join(path, filename)))
-    else:
-        data.update(load_path(path))
-
-    return data
-
-
-def get_sublate_data(path):
-    data = { }
-
-    if os.path.isdir(path):
-        for filename in os.listdir(path):
-            if filename.startswith("sublate."):
-                data.update(load_path(os.path.join(path, filename)))
-    else:
-        data.update(load_path(path))
-
-    return data
-
-
-def load_path(path):
-    if path.endswith(".json"):
-        return load_json(path)
-
-    if path.endswith(".yaml"):
-        return load_yaml(path)
-
-    if path.endswith(".py"):
-        return load_py(path)
-
-
-def load_json(path):
-    with open(path) as f:
-        return json.loads(f.read())
-
-
-def load_yaml(path):
-    with open(path) as f:
-        return yaml.load(f.read(), Loader=yaml.FullLoader)
-
-
-def load_py(path):
+def run(path):
     with open(path) as f:
         module = types.ModuleType('module')
 
@@ -200,5 +31,69 @@ def load_py(path):
         return data
 
 
-if __name__ == "__main__":
-    main()
+# shell
+
+
+def cp(source, target):
+    shutil.copytree(source, target) 
+
+
+def mkdir(path):
+    os.makedirs(path)
+
+
+def rm(path):
+    shutil.rmtree(path, ignore_errors=True)
+
+
+# template
+
+
+def render(path, template, template_data={}):
+    env = jinja2.Environment(loader=jinja2.ChoiceLoader([
+        jinja2.FileSystemLoader(searchpath="."),
+        jinja2.FileSystemLoader(searchpath=".")
+    ]))
+
+    _template = env.get_template(template)
+
+    output = _template.render(**template_data).strip()
+    with open(path, 'w+') as f:
+        f.write(output)
+
+
+# data
+
+
+def load(path):
+    basename = os.path.basename(path)
+
+    if os.path.isdir(path):
+        for filename in os.listdir(path):
+            _data = _load_path(os.path.join(path, filename))
+    
+            if basename not in data:
+                data[basename] = {}
+
+            name = filename.split(".")
+            data[basename][name[0]] = _data
+    else:
+        # TODO
+        pass
+
+def _load_path(path):
+    if path.endswith(".json"):
+        return _load_json(path)
+
+    if path.endswith(".yaml"):
+        return _load_yaml(path)
+
+
+def _load_json(path):
+    with open(path) as f:
+        return json.loads(f.read())
+
+
+def _load_yaml(path):
+    with open(path) as f:
+        return yaml.load(f.read(), Loader=yaml.FullLoader)
